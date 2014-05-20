6dd16a51ae0c8baa376dd3d75ddcbd0fed0bcc5c
Cristian Popovici <cristi.popovici@gmail.com>
2014-05-20 13:39:46 +0300

    Update retry.scala

--- retry.scala
+++ retry.scala
@@ -3,12 +3,12 @@ import scala.util.Success
 
 object retry {
 
-@annotation.tailrec
-def retry[T](nrOfTimes:Int)(fn: => T):Try[T] =
-  Try(fn) match {
-  	case x:Success[T] => x
-  	case _ if nrOfTimes > 1 => retry(nrOfTimes - 1)(fn)
-  	case f => f
-  }
-  
+  @annotation.tailrec
+  def retry[T](nrOfTimes:Int)(fn: => T):Try[T] =
+    Try(fn) match {
+    	case x:Success[T] => x
+    	case _ if nrOfTimes > 1 => retry(nrOfTimes - 1)(fn)
+    	case f => f
+    }
+    
 }
