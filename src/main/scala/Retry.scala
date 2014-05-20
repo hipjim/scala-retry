9631090d9cc439cf8c55e591a29c5eca8a7c7997
Cristian Popovici <cristi@192-168-0-103.rdsnet.ro>
2014-05-20 16:19:26 +0300

    ongoing work

---
+++ src/main/scala/Retry.scala
@@ -0,0 +1,50 @@
+import scala.util.{Success, Try}
+
+/**
+ * Created by cristi on 5/20/14.
+ */
+trait Retry {
+  def retry[T](fn: => T)(implicit strategy: RetryStrategy): Try[T]
+}
+
+final class RetryImpl extends Retry {
+
+  @annotation.tailrec
+  override def retry[T](fn: => T)(implicit strategy: RetryStrategy): Try[T] =
+    Try(fn) match {
+      case x: Success[T] => x
+      case _ if strategy.shouldRetry() => println("retrying"); retry(fn)(strategy.update())
+      case f => f
+    }
+}
+
+trait RetryStrategy {
+  def shouldRetry(): Boolean
+
+  def update(): RetryStrategy
+}
+
+case class NumberOfRetriesStrategy(numberOfTimes: Int) extends RetryStrategy {
+  override def shouldRetry(): Boolean = numberOfTimes > 0
+
+  override def update(): RetryStrategy = NumberOfRetriesStrategy(numberOfTimes - 1)
+}
+
+case class NumberOfRetriesStrategyWithDelay(numberOfTimes: Int, millis: Long) extends RetryStrategy {
+  override def shouldRetry(): Boolean = numberOfTimes > 0
+
+  override def update(): RetryStrategy = {
+    Thread.sleep(millis)
+    NumberOfRetriesStrategyWithDelay(numberOfTimes - 1, millis)
+  }
+}
+
+object runner extends App {
+  implicit val retryStrategy = NumberOfRetriesStrategyWithDelay(100, 1000)
+
+  val retry = new RetryImpl()
+  val result = retry.retry(1 / 0)
+  println(result)
+}
+
+
