583000654d3baf35720ce0a18127b2239f5d12ef
cristi <popovici@eloquentix.com>
2014-07-03 18:58:12 +0300

    added tests

--- src/test/scala/RetrySpec.scala
+++ src/test/scala/RetrySpec.scala
@@ -11,12 +11,24 @@ class RetrySpec extends FlatSpec with Matchers {
   implicit val retryStrategy =
     Retry.fixedWaitRetry(retryInterval = 1.seconds, maxRetries = 3)
 
-  "A Retry" should "return `Success` in case of a successful operation" in {
+  "A `Retry` " should "return `Success` in case of a successful operation" in {
     Retry(1 / 1) should be(Success(1))
   }
 
-  "A Retry" should "return `Failure` in case of a failed operation" in {
+  "A `Retry` " should "return `Failure` in case of a failed operation" in {
     Retry(1 / 0) should equal(Failure(new ArithmeticException("/ by zero")))
+  }
+
+  "A `Retry` " should "recover in case of a failure `Failure` " in {
+    val result = Retry(1 / 0) recover {
+      case _ => -1
+    }
+    result should be(Success(1))
+  }
+
+  "A `Retry.get` " should " return the computed value" in {
+    val result = Retry(1 / 1).get
+    result should be(1)
   }
 
 }
