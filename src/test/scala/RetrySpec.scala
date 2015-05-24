41d25c47090aea0497d35fc606d97828f934a7b2
cristi <popovici@eloquentix.com>
2015-05-24 18:50:03 +0300

    fix test

--- src/test/scala/RetrySpec.scala
+++ src/test/scala/RetrySpec.scala
@@ -10,7 +10,7 @@ import util.retry.blocking.Failure
 class RetrySpec extends FlatSpec with Matchers {
 
   implicit val retryStrategy =
-    Retry.fixedWait(retryInterval = 1.seconds, maxRetries = 3)
+    Retry.fixedWait(retryDuration = 1.seconds, maxRetries = 3)
 
   "A `Retry` " should "return `Success` in case of a successful operation" in {
     Retry(1 / 1) should be(Success(1))
